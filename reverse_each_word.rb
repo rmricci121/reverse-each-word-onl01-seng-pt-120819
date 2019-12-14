76d1b6814996db6efcf1695d5dcb376f659d78de
Robert M Ricci <rmricci121@gmail.com>
2019-12-14 13:59:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,10 @@
 def reverse_each_word(sentence1)
   original_array = sentence1.split(" ")
   return_array = []
-  original_array.each do|string|
+  original_array.collect do|string|
     return_array << string.reverse
   end
   return_array.join(" ")
 end
 
 
-original_array = string.split(" ")
-  return_array = []
-  original_array.collect do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
